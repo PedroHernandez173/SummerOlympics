6dd3a1eb71cd538389c459601642229d509f11c0
PedroHernandez173 <Phernande0152@gmail.com>
2025-06-05 09:29:37 -0700

    Initial Commit

---
+++ SummerOlympics/SummerOlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerOlympicsApp.swift
+//  SummerOlympics
+//
+//  Created by Student2 on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SummerOlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
